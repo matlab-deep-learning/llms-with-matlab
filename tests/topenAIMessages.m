7e81d37907f0fc3a8db81fcb302c129ab1a5295f
Christopher Creutzig <89011131+ccreutzi@users.noreply.github.com>
2024-06-12 09:59:40 +0100

    minimal and complete test for the backward compatibility function

---
+++ tests/topenAIMessages.m
@@ -0,0 +1,11 @@
+classdef topenAIMessages < matlab.unittest.TestCase
+% Tests for openAIMessages backward compatibility function
+
+%   Copyright 2023-2024 The MathWorks, Inc.
+
+methods(Test)
+	function returnsMessageHistory(testCase)
+		testCase.verifyClass(openAIMessages,"messageHistory");
+	end
+end
+end
