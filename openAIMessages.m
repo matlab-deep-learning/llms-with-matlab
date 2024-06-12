101cb598fb8794647f506310e5b6f0f610c7f1ae
Christopher Creutzig <89011131+ccreutzi@users.noreply.github.com>
2024-06-12 09:42:18 +0100

    Add openAIMessages fallback for backward compatibility

---
+++ openAIMessages.m
@@ -0,0 +1,10 @@
+function msgs = openAIMessages
+%openAIMessages - backward compatibility function
+%
+%   This function only exists for backward compatibility and will be removed
+%   at some time in the future. Please use messageHistory instead.
+
+% Copyright 2024 The MathWorks, Inc.
+
+msgs = messageHistory;
+end
